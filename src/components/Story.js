ef3dd5494097b5e98d0df8ee5a825b788e154d4c
Bandile Danxa <barnez76@gmail.com>
2023-05-18 06:09:03 +0200

    Update story,js and added some few minor changes on other files

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,43 +1,15 @@
-// import { AccountCircle } from "@mui/icons-material"
 
-export default function Story(){
+export default function Story(props){
 
     return(
         <>
             <div className="story">
-                <img src="./images/create-story-icon.png" alt="" />
-            </div>
-            <div className="story">
-                <img src="./images/athula-cover.jpg" className="story-img" alt="" />
+                <img src={props.storyImg} width="114" height="200" className="story-preview-image" alt=""/>
                 <div>
-                    <img src="./images/athula-profile.jpg" className="acc-profile-circle" alt="" />
+                    <img src={props.accImg} width="35" height="35" className="acc-circle" alt=""/>
+                    <p className="acc-label">{props.contactName}</p>
                 </div>
             </div>
-            <div className="story">
-                <img src="./images/tubba-cover.jpg" className="story-img" alt="" />
-                <div>
-                    <img src="./images/tubba-profile.jpg" className="acc-profile-circle" alt="" />
-                </div>
-            </div>
-            <div className="story">
-                <img src="./images/chrisie6.jpg" className="story-img" alt="" />
-                <div>
-                    <img src="./images/chrisie-profile.jpg" className="acc-profile-circle" alt="" />
-                </div>
-            </div>
-            <div className="story">
-                <img src="./images/aya-cover.jpg" className="story-img" alt="" />
-                <div>
-                    <img src="./images/aya-profile.jpg" className="acc-profile-circle" alt="" />
-                </div>
-            </div>
-            {/* <div className="story">
-                <img src="./images/ntsako-profile.jpg" className="story-img" alt="" />
-                <div>
-                    <img src="./images/ntsako-profile.jpg" className="acc-profile-circle" alt="" />
-                </div>
-            </div> */}
         </>
     )
-
 }
